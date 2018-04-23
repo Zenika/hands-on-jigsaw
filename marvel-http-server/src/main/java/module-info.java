0b14cb04fd8f425230f5d97c78c8cab24cf07072
Saïd Bouras <said.bouras@gmail.com>
2018-04-23 21:36:38 +0200

    Migrate to maven

--- marvel-http-server/src/main/java/module-info.java
+++ marvel-http-server/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 open module org.zenika.handson.jigsaw.http {
     requires java.logging;
-    requires org.zenika.handson.jigsaw.api;
+//    requires org.zenika.handson.jigsaw.api;
+    requires org.zenika.handson.jigsaw.api.local.impl;
     requires jdk.httpserver;
     uses org.zenika.handson.jigsaw.api.CharactersApi;
 
